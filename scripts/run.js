c8cd9650bd84d18d74b02a8909ece226fffd0a76
YD <ydglitch@gmail.com>
2022-04-07 20:10:38 +0000

    runJs

--- scripts/run.js
+++ scripts/run.js
@@ -8,10 +8,17 @@ const main = async () => {
 
      let waveCount;
      waveCount = await waveContract.getTotalWaves();
+      console.log(waveCount.toNumber());
 
-     let waveTxn = await waveContract.wave();
+     let waveTxn = await waveContract.wave("the YD message!");
      await waveTxn.wait();
-
+     const [_] = await hre.ethers.getSigners();
+     waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+     await waveTxn.wait(); // Wait for the transaction to be mined
+   
+     let allWaves = await waveContract.getAllWaves();
+     console.log(allWaves);
+     
      waveCount = await waveContract.getTotalWaves();
 
 };
