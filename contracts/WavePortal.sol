b9eb16c5d337408a8d528c1bd1ec2ce393529831
YD <ydglitch@gmail.com>
2022-04-07 19:53:45 +0000

    wave-contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,17 +4,34 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-    constructor() {
-          console.log("Yo yo, I am a contract and I am smart");
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave{
+        address waver;
+        string message;
+        uint timestamp;
     }
 
-    function wave() public {
-        totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
+    Wave[] waves;
+
+    constructor() {
+          console.log("I AM SMART CONTRACT. POG.");
     }
+
+    function wave(string memory _message) public {
+        totalWaves += 1;
+        console.log("%s waved w/ message %s", msg.sender, _message);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+    function getAllwaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
 
     function getTotalWaves() public view returns (uint256){
-        console.log("we have %d total waves!", totalWaves);
+        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 
