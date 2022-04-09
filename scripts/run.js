c007029e3270f53ad6d929f43beda10e822f5bc8
YD <ydglitch@gmail.com>
2022-04-09 09:02:04 +0000

    runJs

--- scripts/run.js
+++ scripts/run.js
@@ -1,25 +1,39 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-     const waveContract = await waveContractFactory.deploy();
-     await waveContract.deployed();
-     console.log("Contract deployed to:", waveContract.address);
-     console.log("Contract deployed by:", owner.address);
-
-     let waveCount;
-     waveCount = await waveContract.getTotalWaves();
-      console.log(waveCount.toNumber());
-
-     let waveTxn = await waveContract.wave("the YD message!");
-     await waveTxn.wait();
-     const [_] = await hre.ethers.getSigners();
-     waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
-     await waveTxn.wait(); // Wait for the transaction to be mined
-   
-     let allWaves = await waveContract.getAllWaves();
-     console.log(allWaves);
-     
-     waveCount = await waveContract.getTotalWaves();
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy({
+      value: hre.ethers.utils.parseEther("0.1"),
+    });
+    await waveContract.deployed();
+    console.log("Contract addy:", waveContract.address);
+  
+    /*
+     * Get Contract balance
+     */
+    let contractBalance = await hre.ethers.provider.getBalance(
+      waveContract.address
+    );
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
+  
+    /*
+     * Send Wave
+     */
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait();
+  
+    /*
+     * Get Contract balance to see what happened!
+     */
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
+  
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 
 };
 
